ac97ec1534bcf64cac1be6e9a87b5795274cbbec
S Foley <smfoley1987@gmail.com>
2015-01-25 15:48:03 -0800

    R class programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Takes the matrix and caches the matrix into a list to allow for cacheSolve to transpose the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+t <- NULL
+set <- function(y) {
+  x <<- y
+  t <<- NULL
+} 
+get <- function() x
+settranspose <- function(transpose) t <<- transpose
+gettranspose <- function() t
+list(set = set, get = get,
+     settranspose = settranspose,
+     gettranspose = gettranspose)
 }
 
 
-## Write a short comment describing this function
+## takes the cached matrix from makeCacheMatrix and displays "getting cached data", 
+## then transposes the matrix. Returns transposed matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  tp <- x$gettranspose()
+  if(!is.null(tp)) {
+    message("getting cached data")
+    return(tp)
+  }
+  data <- x$get()
+  tp <- t(data, ...)
+  x$settranspose(tp)
+  tp
 }
+
+
